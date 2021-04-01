2dc39f0d6bcb38dcefee91b02aefc5f8929a74db
Alexis <Alexismatamoros1996888@gmail.com>
2021-04-01 16:14:12 -0600

    Se cambio el protocolo de la peticion a GIPHY

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@ const getGifs = async (category) =>{
     const api_key  = 'yPjOb6dQt00AfmeD73bIhKkGwDMkGoJw'
     const query = encodeURI(category);
     const limit=10
-    const end_point = `http://${API_URL}?api_key=${api_key}&q=${query}&limit=${limit}`
+    const end_point = `https://${API_URL}?api_key=${api_key}&q=${query}&limit=${limit}`
     
     const resp = await fetch(end_point)
     const {data} = await resp.json();
